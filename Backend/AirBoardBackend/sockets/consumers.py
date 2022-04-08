58b44eff37268726e558848f17026e9c0bcd23f1
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2022-04-08 16:51:02 +0530

    Chat Websocket

--- Backend/AirBoardBackend/sockets/consumers.py
+++ Backend/AirBoardBackend/sockets/consumers.py
@@ -37,3 +37,40 @@ class BoardConsumer(AsyncConsumer):
         )
         raise StopConsumer()
 
+
+
+class ChatConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        self.group_name = str(self.scope['url_route']['kwargs']['team_id'])
+        await self.channel_layer.group_add(
+            self.group_name, 
+            self.channel_name
+        )
+        await self.send({
+            'type': 'websocket.accept'
+        })
+    
+
+    async def websocket_receive(self, event):
+        # data = json.loads(event['text']) # use if we need to store in db
+        await self.channel_layer.group_send(self.group_name, {
+            'type': 'chat.message',
+            'message': event['text'],
+            'sender_channel_name': self.channel_name
+        })
+    
+
+    async def chat_message(self, event):
+        if self.channel_name != event['sender_channel_name']:
+            await self.send({
+                "type": "websocket.send",
+                'text': event['message']
+            })
+
+
+    async def websocket_disconnect(self, event):
+        await (self.channel_layer.group_discard)(
+            self.group_name, 
+            self.channel_name
+        )
+        raise StopConsumer()
