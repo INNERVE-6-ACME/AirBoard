71a51e5316987f69d65366a000a9a0faaaa64ea2
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2022-04-08 13:28:02 +0530

    Backend Models and APIviews

--- Backend/AirBoardBackend/api/urls.py
+++ Backend/AirBoardBackend/api/urls.py
@@ -9,7 +9,9 @@ router.register("teams", views.TeamModelViewSet, basename="teams")
 urlpatterns = [
     path('', include(router.urls)),
     path('signup/', views.NewUser.as_view()),
+    path('addstudent', views.AddRemoveStudent.as_view()),
     path('session/<int:session_id>', views.SessionAPI.as_view(), name='session'),
     path('addsession', views.CreateDeleteSession.as_view()),
-    path('login/', obtain_auth_token, name='login')
+    path('savesession/', views.SaveSession.as_view()),
+    path('login/', obtain_auth_token, name='login'),
 ]
