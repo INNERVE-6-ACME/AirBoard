d804832d566cce6f7eb0b16c217c27e3f7fd3fd2
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2022-04-09 11:17:21 +0530

    a

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -7,7 +7,8 @@ const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 var canvasCtx2 = null
 var canvas2=null
-
+//let canvasCtx2=document.getElementById("#canvas-wrapper").getContext("2d");
+//require("dotenv").config()
 var ws = new WebSocket(ws_url + "/ws/board/" + roomId)
 const camera = new Camera(videoElement, {
   onFrame: async () => {
@@ -38,6 +39,8 @@ function fingersUp(res4, total) {
 function onResults(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.translate(width, 0);
+  canvasCtx.scale(-1, 1);
   canvasCtx.drawImage(
     results.image, 0, 0, canvasElement.width, canvasElement.height);
   if (results.multiHandLandmarks.length !== 0) {
@@ -46,25 +49,33 @@ function onResults(results) {
     //console.log(results.multiHandLandmarks)
     var res2 = results.multiHandLandmarks[0][8]
     var res4 = results.multiHandLandmarks[0][4]
+    //var thumb3= results.multiHandLandmarks[0][3]
+    //var middle = results.multiHandLandmarks[0][12]
+    //var ring= results.multiHandLandmarks[0][16]
+    //var little =results.multiHandLandmarks[0][20]
     var pos = fingersUp(res4, results.multiHandLandmarks)
     if (document.getElementById("defaultCanvas0") != null)
     {
       canvasCtx2 = document.getElementById("defaultCanvas0").getContext("2d");
       canvas2 = document.getElementById("defaultCanvas0")}
-    if (pos[1] === 1 && pos[2] === 1 && pos[3]===0 && pos[4]===0 && canvasCtx2 != null) {
+    if (pos[1] === 1 && pos[2] === 1 && pos[3]===1 && pos[4]===1 && canvasCtx2 != null) {
       // canvasCtx2.beginPath();
       // //console.log(res2.x, res2.y);
       // canvasCtx2.arc((1 - res1.x) * 1280, res1.y * 720, 10, 0, 2 * Math.PI);
       // strokeWeight(trackerSize)
 
-      canvasCtx2.fillStyle = 'white';
-      canvasCtx2.fillRect((1 - res1.x) * 1280 - 20, res1.y * 720 - 20, 40, 40);
+      canvasCtx2.fillStyle = "rgba(0,0,0,0)";
+      canvasCtx2.clearRect((1 - res1.x) * 1280 - 20, res1.y * 720 - 20, 40, 40);
       ws.send(JSON.stringify({
         "type": "erase",
         "c1": { x: (1 - res1.x), y: res1.y },
-        "esize": esize
       }))
 
+      // canvasCtx2.beginPath();
+      // //console.log(res2.x, res2.y);
+      // canvasCtx2.arc((1 - res2.x) * 1280, res2.y * 720, 10, 0, 2 * Math.PI);
+      // strokeWeight(trackerSize)
+      // canvasCtx2.stroke()
     }
     else if(pos[1] === 1 && pos[2] === 0 && pos[3]===0 && pos[4]===0 && canvasCtx2 != null){
       mouseDragged(res1, res2, res4)
@@ -102,6 +113,7 @@ function _(selector) {
 function setup() {
   let canvas = createCanvas(1280, 720);
   canvas.parent("#canvas-wrapper");
+  //background(255);
 }
 
 function mouseDragged(res1, res2, res4) {
@@ -111,18 +123,31 @@ function mouseDragged(res1, res2, res4) {
   //console.log(typeof(color))
   fill(color);
   stroke(color);
+  // var x0 = res4.x * 1280;
+  // var y0 = res4.y * 720;
+  // var x1 = res2.x * 1280;
+  // var y1 = res2.y * 720;
+  // function distance(x0, y0, x1, y1) {
+  //   return Math.hypot(x1 - x0, y1 - y0);
+  // }
+  // var dist = distance(x0, y0, x1, y1)
   if (type == "pencil") {
     let pmouseX = (1 - res1.x) * 1280, pmouseY = res1.y * 720, mouseX = (1 - res2.x) * 1280, mouseY = res2.y * 720
     strokeWeight(size)
+    stroke(color)
     //var mat = cv.imread(canvas2);
     //let p1 = new cv.Point(pmouseX, pmouseY);
     //let p2 = new cv.Point(mouseX, mouseY);
     line(pmouseX, pmouseY, mouseX, mouseY)
+    //cv.line(mat, p1, p2, [0, 255, 0, 255], 1)
+    //console.log(mat)
+    //cv.imshow(canvas2, mat);
     ws.send(JSON.stringify({
       "type": "pen",
       "c1": { x: pmouseX, y: pmouseY },
       "c2": { x: mouseX, y: mouseY },
-      "stroke-size": size
+      "stroke-size": size,
+      "stroke-color": color,
     }))
   }
 }
@@ -130,21 +155,35 @@ function mouseDragged(res1, res2, res4) {
 ws.onmessage = (e) => {
   var x = e
   e = JSON.parse(e.data)
+  //console.log(typeof(e))
+  // console.log(e['c1'])
+  // console.log(e.c1.x)
   if (e["type"] === "pen") {
     strokeWeight(e['stroke-size'])
+    stroke(e['stroke-color'])
     line(e['c1']['x'], e['c1']['y'], e['c2']['x'], e['c2']['y']);
   }
-  else {
-    if (document.getElementById("defaultCanvas0") != null)
+  else if (e["type"] === "erase") {
+    if (document.getElementById("defaultCanvas0") != null) {
       canvasCtx2 = document.getElementById("defaultCanvas0").getContext("2d");
-    canvasCtx2.fillStyle = 'white';
-    canvasCtx2.fillRect((1 - e["c1"]["x"]) * 1280 - 20, e["c1"]["y"] * 720 - 20, 40, 40);
+      canvasCtx2.fillStyle = "rgba(0,0,0,0)";
+      canvasCtx2.clearRect(( e['c1']['x']) * 1280 - 20,e['c1']['y'] * 720 - 20, 40, 40);
+    }
+    console.log(e);
+  }
+  else if(e["type"] == "reset"){
+    clear()
   }
 }
 
 _("#reset-canvas").addEventListener("click",
   function () {
+    // canvasCtx2.fillStyle="rgba(0,0,0,0)"
+    // canvasCtx2.clearRect(0, 0, canvasCtx2.width, canvasCtx2.height);
     clear()
+    ws.send(JSON.stringify({
+      "type": "reset"
+    }))
   });
 
 _("#save-canvas").addEventListener("click",
